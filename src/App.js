ad528d5537ccb6ee4c2b5393dce5fe58750a4772
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2022-03-14 12:27:06 -0400

    Fix react component code lines.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,125 @@
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Two Truths & A Lie</h1>
+        <br />
+        <div className="Form">
+          <form>
+            <label>
+              User Name: <input type="text" name="username" />
+            </label>
+
+            <ul className="FactInputs">
+              <li>
+                <label>
+                  "Fact" 1 <input type="text" />
+                </label>
+                <label>
+                  isLie
+                  <input type="checkbox" />
+                </label>
+              </li>
+
+              <li>
+                <label>
+                  "Fact" 2 <input type="text" />
+                </label>
+                <label>
+                  isLie
+                  <input type="checkbox" />
+                </label>
+              </li>
+
+              <li>
+                <label>
+                  "Fact" 3 <input type="text" />
+                </label>
+                <label>
+                  isLie
+                  <input type="checkbox" />
+                </label>
+              </li>
+            </ul>
+
+            <div className="InfoBottom">
+              <label>
+                Vote <input type="number" />
+              </label>
+              <br />
+              <button>Send "Fact"</button>
+              <button>Send Vote</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//
+//
+// //
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Two Truths & A Lie</h1>
+//       <br />
+//       <div className="Form">
+//         <form>
+//           <label>
+//             User Name: <input type="text" name="username" />
+//           </label>
+
+//           <ul className="FactInputs">
+//             <li>
+//               <label>
+//                 "Fact" 1 <input type="text" />
+//               </label>
+//               <label>
+//                 isLie
+//                 <input type="checkbox" />
+//               </label>
+//             </li>
+
+//             <li>
+//               <label>
+//                 "Fact" 2 <input type="text" />
+//               </label>
+//               <label>
+//                 isLie
+//                 <input type="checkbox" />
+//               </label>
+//             </li>
+
+//             <li>
+//               <label>
+//                 "Fact" 3 <input type="text" />
+//               </label>
+//               <label>
+//                 isLie
+//                 <input type="checkbox" />
+//               </label>
+//             </li>
+//           </ul>
+
+//           <div className="InfoBottom">
+//             <label>
+//               Vote <input type="number" />
+//             </label>
+//             <br />
+//             <button>Send "Fact"</button>
+//             <button>Send Vote</button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
